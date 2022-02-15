34919f793b0bc88cc2971942d0f8ef04f968ca42
berkanturkali <brktrkali@outlook.com>
2022-02-16 01:52:08 +0300

    migrate app/build.gradle groovy to kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    androidApplication
+    kotlinAndroid
+}
+
+android {
+    compileSdk = Config.Version.compileSdkVersion
+
+    defaultConfig {
+        applicationId = Config.Android.applicationId
+        minSdk = Config.Version.minSdkVersion
+        targetSdk = Config.Version.targetSdkVersion
+        versionCode = Config.Version.versionCode
+        versionName = Config.Version.versionName
+        multiDexEnabled = Config.isMultiDexEnabled
+        testInstrumentationRunner = Config.Android.testInstrumentationRunner
+    }
+
+    buildTypes {
+        named(BuildType.DEBUG) {
+            isMinifyEnabled = false
+            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+        }
+        named(BuildType.RELEASE) {
+            isMinifyEnabled = true
+            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation(Dependencies.AndroidX.coreKtx)
+    implementation(Dependencies.AndroidX.appCompat)
+    implementation(Dependencies.View.material)
+    implementation(Dependencies.View.constraintLayout)
+    testImplementation(Dependencies.Test.junit)
+    androidTestImplementation(Dependencies.Test.junitExt)
+    androidTestImplementation(Dependencies.Test.espresso)
+}
