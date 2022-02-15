bdc85743377022d3eb6dfd095c0ab88d0d8648c7
berkanturkali <brktrkali@outlook.com>
2022-02-16 02:21:20 +0300

    modify ApiService

--- core/src/main/java/com/example/nesineassignment/core/remote/service/ApiService.kt
+++ core/src/main/java/com/example/nesineassignment/core/remote/service/ApiService.kt
@@ -1,7 +1,10 @@
 package com.example.nesineassignment.core.remote.service
 
+import com.example.nesineassignment.core.remote.model.PostDto
 import retrofit2.http.GET
 
 interface ApiService {
 
+    @GET("posts")
+    suspend fun fetchPosts(): List<PostDto>
 }
