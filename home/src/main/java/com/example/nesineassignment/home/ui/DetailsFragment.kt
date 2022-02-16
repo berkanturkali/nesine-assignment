6571aa70d2ddb395c6a42c8d503896b4b262dcc3
berkanturkali <brktrkali@outlook.com>
2022-02-16 20:13:11 +0300

    add Details Fragment & DetailsFragmentViewModel

---
+++ home/src/main/java/com/example/nesineassignment/home/ui/DetailsFragment.kt
@@ -0,0 +1,69 @@
+package com.example.nesineassignment.home.ui
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.transition.Slide
+import androidx.transition.TransitionManager
+import com.example.nesineassignment.common.BaseFragment
+import com.example.nesineassignment.core.domain.model.Post
+import com.example.nesineassignment.home.R
+import com.example.nesineassignment.home.databinding.FragmentDetailsBinding
+import com.example.nesineassignment.home.viewmodel.DetailsFragmentViewModel
+
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(FragmentDetailsBinding::inflate) {
+
+    private val mViewModel by viewModels<DetailsFragmentViewModel>()
+
+
+    private val args by navArgs<DetailsFragmentArgs>()
+
+    private lateinit var post: Post
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailsBinding.bind(view)
+        binding.editTitleBtn.setOnClickListener {
+            showDialog(post.title,true)
+        }
+        binding.editBodyBtn.setOnClickListener {
+            showDialog(post.body,false)
+        }
+        binding.updateBtn.setOnClickListener {
+            mViewModel.updatePost(post)
+        }
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+        mViewModel.post.observe(viewLifecycleOwner) {
+            animateVisibility(args.post != it)
+            post = it
+            binding.post = it
+        }
+
+        mViewModel.updated.observe(viewLifecycleOwner){
+            it.getContentIfNotHandled()?.let { success->
+                if(success) findNavController().navigateUp()
+            }
+        }
+    }
+
+    private fun showDialog(titleBody: String,isTitle:Boolean) {
+        val action = DetailsFragmentDirections.actionDetailsFragmentToEditDialog(titleBody,isTitle)
+        findNavController().navigate(action)
+    }
+
+    private fun animateVisibility(visible:Boolean){
+        val transition = Slide(Gravity.BOTTOM)
+        transition.apply {
+            duration = 600
+            addTarget(binding.updateBtn)
+        }
+        TransitionManager.beginDelayedTransition(binding.rootView,transition)
+        binding.updateBtn.visibility = if(visible) View.VISIBLE else View.GONE
+    }
+}
---
+++ home/src/main/java/com/example/nesineassignment/home/viewmodel/DetailsFragmentViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.nesineassignment.home.viewmodel
+
+import androidx.lifecycle.*
+import com.example.nesineassignment.core.domain.Event
+import com.example.nesineassignment.core.domain.executor.abstraction.PostExecutionThread
+import com.example.nesineassignment.core.domain.model.Post
+import com.example.nesineassignment.core.domain.repo.PostsRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsFragmentViewModel @Inject constructor(
+    private val repo: PostsRepo,
+    private val savedStateHandle: SavedStateHandle,
+    private val postExecutionThread: PostExecutionThread
+): ViewModel() {
+
+    private val _post = MutableLiveData<Post>()
+
+    val post: LiveData<Post> get() = _post
+
+    private val _updated = MutableLiveData<Event<Boolean>>()
+    val updated: LiveData<Event<Boolean>> get() = _updated
+
+    init {
+        savedStateHandle.get<Post>("post")?.let {
+            setPost(it)
+        }
+    }
+
+    private fun setPost(post:Post){
+        _post.value = post
+    }
+
+    fun updateTitle(title:String){
+        _post.value = _post.value?.copy(title = title)
+    }
+
+    fun updateBody(body:String){
+        _post.value = _post.value?.copy(body = body)
+    }
+
+    fun updatePost(post:Post){
+
+    }
+}
