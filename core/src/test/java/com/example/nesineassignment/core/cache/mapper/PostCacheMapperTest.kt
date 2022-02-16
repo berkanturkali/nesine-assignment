f4fdd51756c927dec73970caa18270712a178bb7
berkanturkali <brktrkali@outlook.com>
2022-02-16 13:19:18 +0300

    test PostCacheMapper

--- core/src/test/java/com/example/nesineassignment/core/cache/mapper/PostCacheMapperTest.kt
+++ core/src/test/java/com/example/nesineassignment/core/cache/mapper/PostCacheMapperTest.kt
@@ -1,7 +1,26 @@
 package com.example.nesineassignment.core.cache.mapper
 
-import org.junit.Assert.*
+import com.example.nesineassignment.core.BuildConfig
+import com.example.nesineassignment.core.util.DummyData
+import com.google.common.truth.Truth
+import org.junit.Test
 
-class PostCacheMapperTest{
+class PostCacheMapperTest {
 
+    private val mapper = PostCacheMapper()
+
+    @Test
+    fun `check that map from entity map data correctly`() {
+        val entity = DummyData.postEntity
+        val domain = mapper.mapFromEntity(entity)
+        Truth.assertThat(domain.image)
+            .isEqualTo("${BuildConfig.BASE_IMAGE_URL}/id/${entity.id}/200/")
+    }
+
+    @Test
+    fun `check that map to entity map data correctly`(){
+        val domain = DummyData.post
+        val entity = mapper.mapToEntity(domain)
+        Truth.assertThat(domain.id).isEqualTo(entity.id)
+    }
 }
--- core/src/test/java/com/example/nesineassignment/core/util/DummyData.kt
+++ core/src/test/java/com/example/nesineassignment/core/util/DummyData.kt
@@ -1,5 +1,8 @@
 package com.example.nesineassignment.core.util
 
+import com.example.nesineassignment.core.BuildConfig
+import com.example.nesineassignment.core.cache.model.PostEntity
+import com.example.nesineassignment.core.domain.model.Post
 import com.example.nesineassignment.core.remote.model.PostDto
 
 internal object DummyData {
@@ -12,5 +15,26 @@ internal object DummyData {
         title = "dolorem dolore est ipsam",
         id = 8,
         userId = 1
+    )
+
+    val postEntity = PostEntity(
+        body = "    val body: String,\n" +
+                "    val id: Int,\n" +
+                "    val title: String,\n" +
+                "    val userId: Int",
+        title = "dolorem dolore est ipsam",
+        id = 8,
+        userId = 1
+    )
+
+    val post = Post(
+        body = "    val body: String,\n" +
+                "    val id: Int,\n" +
+                "    val title: String,\n" +
+                "    val userId: Int",
+        title = "dolorem dolore est ipsam",
+        id = 8,
+        userId = 1,
+        image = "${BuildConfig.BASE_IMAGE_URL}/id/8/200/"
     )
 }
