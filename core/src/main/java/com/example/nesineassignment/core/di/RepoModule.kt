bf366d9e174eab066085db3d3c4e08574f15c95e
berkanturkali <brktrkali@outlook.com>
2022-02-16 18:35:03 +0300

    add RepoModule

---
+++ core/src/main/java/com/example/nesineassignment/core/di/RepoModule.kt
@@ -0,0 +1,15 @@
+package com.example.nesineassignment.core.di
+
+import com.example.nesineassignment.core.data.repo.PostsRepoImpl
+import com.example.nesineassignment.core.domain.repo.PostsRepo
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@[Module InstallIn(SingletonComponent::class)]
+interface RepoModule {
+
+    @get:Binds
+    val PostsRepoImpl.postsRepo: PostsRepo
+}
